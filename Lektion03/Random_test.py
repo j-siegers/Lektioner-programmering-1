6774d62a3a2f5a31a5c348ba13a977050acf3894
Johan Siegers <j-siegers@outlook.com>
2023-09-02 19:16:31 +0200

    Nya övningsuppgifter

---
+++ Lektion03/Random_test.py
@@ -0,0 +1,28 @@
+import random
+p = random.random()
+print(p)
+if p < 0.21:
+  print('Du vann något!')
+else:
+  print('Ingen vinst.')
+'''
+vokaler = ['a','A','e','E','i','I','o','O','u','U','y','Y','å','Å','ä','Ä','ö','Ö']
+sträng = input('Skriv ett ord: ')
+ant_vokaler = 0
+#tips: för att se om ett tecken är en vokal skriv t.ex.:
+#if tecken in vokaler:
+for i, tecken in enumerate(sträng):
+    if tecken in vokaler:
+        ant_vokaler += 1
+print('Strängen innehåller', ant_vokaler, 'vokaler')
+'''
+färger = ['blå', 'svart', 'gul', 'röd', 'violett', 'grön', 'orange', 'grå', 'vit']
+print(len(färger))
+while True:
+  start = input('Vill du få en färg? (j/n) ')
+  if start == 'j':
+    rand = random.randint(0, (len(färger)-1))
+    print(färger[rand])
+  else:
+    print('Hejdå')
+    break
--- Lektion03/ex_32.py
+++ Lektion03/ex_32.py
@@ -10,19 +10,19 @@ elif ålder == 3:
     sömn = 12
 elif ålder == 4:
     sömn = 11.5
-elif ålder == 5 or 6:
+elif ålder > 4 and ålder < 7:
     sömn = 11
 elif ålder == 7:
     sömn = 10.5
-elif ålder == 8 or 9 or 10:
+elif ålder > 7 and ålder < 10:
     sömn = 10
 elif ålder == 11:
     sömn = 9.5
-elif ålder == 12 or 13 or 14 or 15:
+elif ålder > 11 and ålder < 16:
     sömn = 9
 elif ålder == 16:
     sömn = 8.5
 elif ålder >= 17:
     sömn = 8
 
-print('Hallå', namn, '! Enligt Vårdguidens rekommendationer behöver individer i din ålder (', ålder, ') sova minst ', sömn, 'timmar per natt.')
+print('Hallå',namn,'! Enligt Vårdguidens rekommendationer behöver individer i din ålder (',ålder,') sova minst ',sömn, 'timmar per natt.')
