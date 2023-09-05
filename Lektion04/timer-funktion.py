f9a0753e46b34b8043f352a98fe148ba26c23b48
Johan Siegers <j-siegers@outlook.com>
2023-09-05 21:33:34 +0200

    Övningsuppgifter från kapitel 7-8

---
+++ Lektion04/timer-funktion.py
@@ -0,0 +1,10 @@
+import time
+for i in range(10):
+    print(str(i), end='')
+    time.sleep(0.5)
+print('\n')
+name = 'Lars'
+age = 50
+print(f'{name} är {age} år gammal')
+for i in range(10):
+    print(i)
