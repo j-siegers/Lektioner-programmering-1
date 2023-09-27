f31e382776c6e3a4cc6e7d9ede91a601aceee501
Johan Siegers <j-siegers@outlook.com>
2023-09-27 15:14:30 +0200

    21 uppdaterad

--- Inlämningsuppgift/tjugoett.py
+++ Inlämningsuppgift/tjugoett.py
@@ -26,9 +26,9 @@ class Card:
                 card_list.append([c_name, c_value])
         return card_list
 
+    # Tar emot kortleken och blandar slumpmässigt alla kort
     @staticmethod
     def card_deck_shuffle(kortlek: list):
-        # Tar emot kortleken och blandar slumpmässigt alla kort
         shuffle(kortlek)
         return kortlek
 
@@ -39,7 +39,7 @@ class Hand:
     @staticmethod
     def kortlek():
         # Korten dras ett i taget från slutet av kortleken för att lättare matcha rätt indexvärde
-        # vid borttagning av dragna kort
+        # vid borttagning av dragna kort.
         # Värdena sätts till globala för att stanna kvar utanför metoden
         global spelar_poang
         global i
@@ -72,6 +72,39 @@ class Hand:
             elif kort_val == 'n':
                 break
 
+    @staticmethod
+    def dator_kort():
+        global dator_poang
+        global i
+
+        while i > -1:
+            print(f'Datorn drog: {kortlek_blandad[i][0]} {kortlek_blandad[i][1]}')
+
+            # Om kortet inte har en siffra hämtas värdet från dictionaryt kort_varden
+            if kortlek_blandad[i][1] in kort_varden:
+                dator_poang += kort_varden[kortlek_blandad[i][1]]
+            # Datorval om kortet är ett ess
+            elif kortlek_blandad[i][1] == 'Ess':
+                # Datorn väljer värde utifrån poäng hittils
+                # ess_val = input('Välj värde: 1 eller 14 > ')
+                if dator_poang > 7:
+                    dator_poang += 1
+                else:
+                    dator_poang += 14
+
+            else:
+                dator_poang += int(kortlek_blandad[i][1])
+
+            kortlek_blandad.pop(i)  # Kortet tas bort från kortleken
+            i -= 1  # Nedräkning av index för loopen
+            print(f'Datorns poäng är: {dator_poang}')
+            print('-' * ui_width)
+            # Valmekanism för om datorn ska fortsätta eller inte
+            if dator_poang >= 17:
+                break
+            else:
+                continue
+
 
 # Funktion för rensande av skärmen
 def clear():
@@ -99,7 +132,11 @@ kort_varden = {
 }
 # Spelarens poäng
 spelar_poang = 0
-i = 51  # Räknare för kortleken
+# Datorns poäng
+dator_poang = 0
+# Räknare för kortleken
+i = 51
+
 ui_width = 30
 
 # UI skrivs ut
@@ -115,8 +152,7 @@ while True:
     print('| 1 | Börja spela')
     print('| 2 | Avsluta')
     print('-' * ui_width)
-    print(f'| Din poäng: {spelar_poang}')
-    print('-' * ui_width)
+
     val = input('> ')
 
     if val == '1':
@@ -125,16 +161,33 @@ while True:
             print(f'\nDatorn vann! Du fick {spelar_poang} poäng')
             print('-' * ui_width)
             input('Tryck Enter för att fortsätta')
-            spelar_poang = 0
+
         elif spelar_poang == 21:
             print(f'\nDu vann! Du fick {spelar_poang} poäng')
+            input('Tryck Enter för att börja om')
 
         elif spelar_poang < 21:
             clear()
             print(f'\nDu fick {spelar_poang} poäng')
             print('Nu är det datorns tur.')
-            input('Enter')
+            Hand.dator_kort()
+            if dator_poang > 21:
+                print('Grattis du vann!')
+                input('Tryck Enter för att börja om')
+                continue
+            elif spelar_poang > dator_poang:
+                print(f'Grattis du vann! Du fick {spelar_poang} poäng')
+                input('Tryck Enter för att börja om')
+                continue
+            else:
+                print(f'Datorn vann med {dator_poang} poäng.\nDu fick {spelar_poang} poäng')
+                input('Tryck Enter för att börja om')
 
+        # Nollställning av poäng inför nästa omgång
+        spelar_poang = 0
+        dator_poang = 0
+
+    # Avslut av programmet
     elif val == '2':
         print('Tack för att du spelade Tjugoett!')
         print(len(kortlek_blandad))
